4d914f33134a98e126dd35d1c4973affc973ab19
dnang36 <nguyendoandangndd@gmail.com>
2022-09-05 11:50:34 +0700

    commit

---
+++ vendor/composer/installed.php
@@ -0,0 +1,23 @@
+<?php return array(
+    'root' => array(
+        'pretty_version' => 'dev-main',
+        'version' => 'dev-main',
+        'type' => 'library',
+        'install_path' => __DIR__ . '/../../',
+        'aliases' => array(),
+        'reference' => '9429d1381a750263d7ee4b1d7dc52f5e30905576',
+        'name' => 'dang/oop',
+        'dev' => true,
+    ),
+    'versions' => array(
+        'dang/oop' => array(
+            'pretty_version' => 'dev-main',
+            'version' => 'dev-main',
+            'type' => 'library',
+            'install_path' => __DIR__ . '/../../',
+            'aliases' => array(),
+            'reference' => '9429d1381a750263d7ee4b1d7dc52f5e30905576',
+            'dev_requirement' => false,
+        ),
+    ),
+);
